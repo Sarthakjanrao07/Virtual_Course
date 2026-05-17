c7525e141b6c0fdaa2629bd1b371aae12559851b
Sarthak Janrao <sarthakjanrav@gmail.com>
2026-05-17 10:24:58 +0530

    Silenced 'no token' toasts and optimized fetch hooks

--- frontend/src/customHooks/getCreatorCourseData.jsx
+++ frontend/src/customHooks/getCreatorCourseData.jsx
@@ -6,27 +6,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 
 const getCreatorCourseData = () => {
-    const dispatch = useDispatch()
-    const {userData} = useSelector(state=>state.user)
+  const dispatch = useDispatch()
+  const { userData } = useSelector(state => state.user)
   return (
-    useEffect(()=>{
-    const getCreatorData = async () => {
-      try {
-        const result = await axios.get(serverUrl + "/api/course/getcreatorcourses" , {withCredentials:true})
-        
-         await dispatch(setCreatorCourseData(result.data))
-
-        
-        console.log(result.data)
-        
-      } catch (error) {
-        console.log(error)
-        toast.error(error.response.data.message)
+    useEffect(() => {
+      const getCreatorData = async () => {
+        if (!userData) return; // Don't fetch if no user is logged in
+        try {
+          const result = await axios.get(serverUrl + "/api/course/getcreatorcourses", { withCredentials: true })
+          await dispatch(setCreatorCourseData(result.data))
+          console.log(result.data)
+        } catch (error) {
+          console.log(error)
+          // Only show toast if it's NOT a token error
+          if (error.response?.data?.message !== "user doesn't have token") {
+            toast.error(error.response?.data?.message || "Something went wrong")
+          }
+        }
       }
-      
-    }
-    getCreatorData()
-  },[userData])
+      getCreatorData()
+    }, [userData])
   )
 }
 
